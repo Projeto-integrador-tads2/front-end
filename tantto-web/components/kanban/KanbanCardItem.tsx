6ea60aee431a0c887123e6455e9f86bf61aecec4
Felipe Marques <felipemarques.computacao@gmail.com>
2025-12-11 21:50:14 -0300

    feat: remove loading spinner from prediction state in Kanban card

--- tantto-web/components/kanban/KanbanCardItem.tsx
+++ tantto-web/components/kanban/KanbanCardItem.tsx
@@ -193,7 +193,6 @@ export function KanbanCardItem({
         >
           {isPredicting ? (
             <>
-              <span className="animate-spin mr-2">⏳</span>
               Prevendo...
             </>
           ) : (
